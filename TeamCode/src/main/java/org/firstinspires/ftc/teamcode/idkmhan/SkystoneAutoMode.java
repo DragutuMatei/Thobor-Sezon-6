5bb3a69599824463991ff27f8c0e104a86398c5e
n0rmalp3rs0n <arandojust@gmail.com>
2021-10-22 14:56:14 +0300

    o alta varianta

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/idkmhan/SkystoneAutoMode.java
@@ -0,0 +1,66 @@
+package org.firstinspires.ftc.teamcode.idkmhan;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+
+@Autonomous(name = "Skystone Detecotor", group = "Auto")
+public class SkystoneAutoMode extends LinearOpMode {
+    OpenCvCamera phoneCam;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        int cameraMonitorViewId = hardwareMap.appContext
+                .getResources().getIdentifier("cameraMonitorViewId",
+                        "id", hardwareMap.appContext.getPackageName());
+        phoneCam = OpenCvCameraFactory.getInstance()
+                .createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
+        Dector detector = new Dector(telemetry);
+        phoneCam.setPipeline(detector);
+//        phoneCam.openCameraDeviceAsync(
+////                () -> phoneCam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT)
+////                OpenCvCamera.AsyncCameraCloseListener cameraCloseListener
+//                OpenCvCamera.AsyncCameraOpenListener cameraOpenListener
+//
+//        );
+
+        phoneCam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                phoneCam.startStreaming(320, 240, OpenCvCameraRotation.SIDEWAYS_LEFT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });
+
+        waitForStart();
+        switch (detector.getLocation()) {
+            case LEFT:
+                // ...
+                telemetry.addLine(String.valueOf(Dector.Location.LEFT));
+                telemetry.update();
+                break;
+            case RIGHT:
+
+                telemetry.addLine(String.valueOf(Dector.Location.RIGHT));
+                telemetry.update();
+                // ...
+                break;
+            case NOT_FOUND:
+                telemetry.addLine(String.valueOf(Dector.Location.NOT_FOUND));
+                telemetry.update();
+                // ...
+        }
+        phoneCam.stopStreaming();
+    }
+}
